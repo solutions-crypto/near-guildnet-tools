945e6aa671ad425ea0747cae77c9476fa0b8354f
crypto-guys <50590698+crypto-guys@users.noreply.github.com>
2020-12-20 03:26:46 -0600

    check for snapd

--- install.sh
+++ install.sh
@@ -53,11 +53,14 @@ function update_via_apt
 {
     echo "* Updating via APT and installing required packages"
     apt-get -qq update && apt-get -qq upgrade
-    apt-get -qq install snapd squashfs-tools 
-    sleep 5
+    snap=$(apt list snapd | grep installed)
+    if [ -z "$snap" ]
+    then
+    apt install -y -q snapd squashfs-tools
+    fi
+    sleep 2
     echo '* Install lxd using snap'
     snap install lxd
-
 }
 
 # Initializes the container software with preseed information.
