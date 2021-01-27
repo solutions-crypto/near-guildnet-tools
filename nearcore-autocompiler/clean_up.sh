63d0403ee5e8b94bdcb1c0d2acd2a3f47750828c
crypto-solutions <74749660+crypto-solutions-net@users.noreply.github.com>
2021-01-27 00:29:46 -0600

    Create clean_up.sh

---
+++ nearcore-autocompiler/clean_up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script will remove everything the install script installs. 
+# It will allow install.sh to be run again and again on the same machine if needed
+
+sudo snap remove lxd --purge
+sudo userdel neard
+sudo groupdel near
+sudo rm -rf /home/neard/.near/guildnet
+sudo systemctl stop neard
+sudo systemctl disable neard
+sudo rm /usr/local/bin/neard
