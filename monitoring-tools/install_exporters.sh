2cf72dbc92eef9546cc59a37ae215d49a51af137
crypto-guys <jasonrpersonal76@gmail.com>
2021-02-04 02:54:23 -0600

    working

--- monitoring-tools/install_exporters.sh
+++ monitoring-tools/install_exporters.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 # Get Prometheus Node Exporter and extract the binary to /usr/loca/bin
-wget -P /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
-tar -xzf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64/node_exporter
-sudo cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin
+#wget -P /tmp/ https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+#tar -xzf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64/node_exporter
+#sudo cp /tmp/node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin
 
+if grep -q monitoring /etc/group
+then
+        echo "group 'monitoring' exists"
+else
+        groupadd monitoring
+fi
+
+# Adding an unprivileged user for the exporter services
+if grep -q exporter /etc/passwd
+then
+        echo "account 'exporter' exists"
+else
+    adduser --system --home /home/exporter --disabled-login --ingroup monitoring exporter || true
+fi
 # Create the unit file
 sudo cat > /etc/systemd/system/node_exporter.service <<EOF
 
