0ed9d698b29c5d5575a6a239aa0dc0bd6f1636d7
crypto-guys <50590698+crypto-guys@users.noreply.github.com>
2021-04-14 07:16:43 -0500

    Update compiler.sh

--- nearcore-autocompiler/compiler.sh
+++ nearcore-autocompiler/compiler.sh
@@ -3,7 +3,7 @@ set -eu
 # Script settings
 RELEASE=$(lsb_release -c -s)
 # Change this to compile a different
-NEAR_VERSION="1.17.0-rc.5"
+NEAR_VERSION="1.18.1-rc.2"
 # Change this to use a different repo
 NEAR_REPO="https://github.com/near-guildnet/nearcore.git"
 NODE_EXPORTER_REPO="https://github.com/prometheus/node_exporter.git"
