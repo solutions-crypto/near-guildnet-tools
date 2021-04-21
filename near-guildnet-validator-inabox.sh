8003832487e1ff185a6f6fab125ab96d45af51ea
crypto-guys <50590698+crypto-guys@users.noreply.github.com>
2021-04-21 18:27:58 -0500

    updating

---
+++ near-guildnet-validator-inabox.sh
@@ -0,0 +1,230 @@
+#!/bin/bash
+
+set -eu
+# Script settings 
+RELEASE=$(lsb_release -c -s) # focal or bionic
+NEAR_VERSION="1.18.3"
+NEAR_REPO="https://github.com/near/nearcore.git"
+NODE_EXPORTER_REPO="https://github.com/prometheus/node_exporter.git"
+NEAR_EXPORTER_REPO="https://github.com/masknetgoal634/near-prometheus-exporter.git"
+vm_name=compiler
+validator_name=validator
+
+echo "* Starting the NEARCORE compile process"
+
+function update_via_apt {
+    echo "* Updating via APT and installing required packages"
+    apt-get -qq update && apt-get -qq upgrade
+#    apt-get -qq install snapd squashfs-tools git curl python3
+
+#    echo '* Install lxd using snap'
+#    snap install lxd
+#    usermod -aG lxd "$USER"
+#    systemctl restart snapd
+#    sleep 5
+#    snap restart lxd
+    sleep 5
+    init_lxd
+#
+}
+
+function init_lxd {
+    echo "* Init LXD With Preseed ---> https://linuxcontainers.org/lxd/docs/master/preseed  "
+    echo "* Cloud init + lxd examples  ---> https://github.com/lxc/lxd/issues/3347 "
+#    cat <<EOF | lxd init --auto
+#EOF
+#    systemctl restart snapd
+#    sleep 5
+}
+
+function create_container_profiles {
+    
+    echo "
+    #cloud-init
+    #test" > cloud.profile
+
+}
+
+function get_container {
+    echo "* Detected Ubuntu $RELEASE"
+    if [ "$RELEASE" == "focal" ]
+    then
+    echo "* Launching Ubuntu $RELEASE Cloud Image"
+    lxc launch images:ubuntu/focal/cloud/amd64 ${vm_name}
+    prepare_container
+    fi
+
+    if [ "$RELEASE" == "bionic" ]
+    then
+    echo "* Launching Ubuntu $RELEASE Cloud Image"
+    lxc launch images:ubuntu/18.04/cloud/amd64 ${vm_name}
+    prepare_container
+    fi
+}
+
+function create_validator_container {
+
+    echo "* Detected Ubuntu $RELEASE"
+    if [ "$RELEASE" == "focal" ]
+    then
+    echo "* Launching Ubuntu $RELEASE Cloud Image"
+    lxc launch images:ubuntu/focal/cloud/amd64 ${validator_name}
+    prepare_validator
+    fi
+
+    if [ "$RELEASE" == "bionic" ]
+    then
+    echo "* Launching Ubuntu $RELEASE Cloud Image"
+    lxc launch images:ubuntu/18.04/cloud/amd64 ${validator_name}
+    prepare_validator
+    fi
+}
+
+function prepare_validator {
+    echo "* Pausing for 5 seconds while the container initializes"
+    sleep 5
+
+    echo "* Configuring the validator container"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq update"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq upgrade"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq autoremove"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq autoclean"
+    echo "* The container is ready for use"
+}
+
+function prepare_container {
+    echo "* Pausing for 5 seconds while the container initializes"
+    sleep 5
+
+    echo "* Configuring the container with all required development tools"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq update"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq upgrade"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq autoremove"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq autoclean"
+    lxc exec ${vm_name} -- sh -c "apt-get -qq install git snapd curl libclang-dev build-essential llvm runc gcc g++ g++-multilib make cmake clang pkg-config libssl-dev libudev-dev libx32stdc++6-7-dbg lib32stdc++6-7-dbg python3-dev"
+    lxc exec ${vm_name} -- sh -c "snap install rustup --classic"
+    lxc exec ${vm_name} -- sh -c "snap install go --classic"
+    lxc exec ${vm_name} -- sh -c "rustup default nightly"
+    lxc exec ${vm_name} -- sh -c "rustup update"
+    echo "* The container is ready for use"
+}
+
+
+function compile {
+    echo "* Cloning the github source"
+    lxc exec ${vm_name} -- sh -c "rm -rf /tmp/src && mkdir -p /tmp/src/ && git clone ${NEAR_REPO} /tmp/src/nearcore"
+    lxc exec ${vm_name} -- sh -c "git clone ${NEAR_EXPORTER_REPO} /tmp/src/near-prometheus-exporter"
+    lxc exec ${vm_name} -- sh -c "git clone ${NODE_EXPORTER_REPO} /tmp/src/node-exporter"
+    echo "* Switching Version"
+    lxc exec ${vm_name} -- sh -c "cd /tmp/src/nearcore && git checkout $NEAR_VERSION"
+    echo "* Attempting to compile nearcore"
+    lxc exec ${vm_name} -- sh -c "cd /tmp/src/nearcore && cargo build -p neard --release"
+    echo "* Attempting to compile Near Prometheus Exporter"
+    lxc exec ${vm_name} -- sh -c "cd /tmp/src/near-prometheus-exporter && go build -a -installsuffix cgo -ldflags="-w -s" -o main ."
+    echo "* Attempting to compile Prometheus Node Exporter"
+    lxc exec ${vm_name} -- sh -c "cd /tmp/src/node-exporter/ && make "
+    lxc exec ${vm_name} -- sh -c "mkdir -p /tmp/src/node-exporter/node_exporter_binaries && cp -r /tmp/src/node-exporter/docs /tmp/src/node-exporter/node_exporter_binaries"
+    lxc exec ${vm_name} -- sh -c "cp -r /tmp/src/node-exporter/examples /tmp/src/node-exporter/node_exporter_binaries && cp -r /tmp/src/node-exporter/node_exporter /tmp/src/node-exporter/node_exporter_binaries"
+    lxc exec ${vm_name} -- sh -c "cp /tmp/src/node-exporter/*.yml /tmp/src/node-exporter/node_exporter_binaries && cp /tmp/src/node-exporter/*.md /tmp/src/node-exporter/node_exporter_binaries"
+    lxc exec ${vm_name} -- sh -c "cp -r /tmp/src/node-exporter/text_collector_examples/ /tmp/src/node-exporter/node_exporter_binaries && cp -r /tmp/src/node-exporter/tls_config_noAuth.bad.yml /tmp/src/node-exporter/node_exporter_binaries"
+    lxc exec ${vm_name} -- sh -c "tar -cjf /tmp/src/node-exporter/node_exporter.tar.gz -C /tmp/src/node-exporter/ node_exporter_binaries"
+}
+
+function get_binary {
+    echo "* Retriving the binary files"
+    mkdir -p /tmp/binaries/
+    lxc file pull -p ${vm_name}/tmp/src/nearcore/target/release/neard /tmp/binaries/
+    lxc file pull -p ${vm_name}/tmp/src/near-prometheus-exporter/near-exporter /tmp/binaries/
+    lxc file pull -p ${vm_name}/tmp/src/node-exporter/node_exporter.tar.gz /tmp/binaries/
+    lxc file pull -p ${vm_name}/tmp/src/node-exporter/node_exporter /tmp/binaries/
+}
+
+function put_binary {
+
+    echo "* Pushing the binary files to the validator container"
+    lxc file push /tmp/binaries/neard ${validator_name}/usr/local/bin/neard
+    create_neard_service
+
+}
+
+function create_neard_service {
+
+    echo "* Creating systemd unit file for NEAR validator service"
+
+    $SERVICE=$(echo "
+    [Unit]
+    Description=NEAR Validator Service
+    Documentation=https://github.com/nearprotocol/nearcore
+    Wants=network-online.target
+    After=network-online.target
+
+    [Service]
+    Type=exec
+    User=neard
+    ExecStart=neard --home /home/neard/.near/guildnet run
+    Restart=on-failure
+    RestartSec=80
+    StandardOutput=append:/var/log/guildnet.log
+
+    [Install]
+    WantedBy=multi-user.target
+    ")
+    lxc exec ${validator_name} -- sh -c "echo $SERVICE > /etc/systemd/system/neard.service"
+
+}
+
+function enable_neard_service {
+
+    lxc exec ${validator_name} -- sh -c "systemctl daemon-reload"
+    lxc exec ${validator_name} -- sh -c "systemctl enable neard"
+
+}
+
+function start_neard {
+
+    lxc exec ${validator_name} -- sh -c "systemctl start neard"
+
+}
+
+function stop_neard {
+
+    lxc exec ${validator_name} -- sh -c "systemctl stop neard"
+
+}
+
+if [ "$USER" != "root" ]
+then
+    echo " You must run the compile script using:  sudo ./compiler.sh "
+    exit
+fi
+function create_compiler_container {
+
+    update_via_apt
+    get_container
+
+}
+
+function compile_neard {
+
+    compile
+    get_binary
+
+}
+function create_validator {
+
+
+}
+
+
+function auto_installer {
+
+    create_compiler_container
+    compile_neard
+    
+    create_validator_container 
+    create_validator
+    
+}
+
+# echo "** The binary files are located in /tmp/binaries"
+# echo "** Please refer to the readme file for further instructions**"
